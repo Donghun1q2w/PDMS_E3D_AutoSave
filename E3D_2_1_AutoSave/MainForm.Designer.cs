f9de80031e1abc8a4e3e56aa93c81e5a7c262bb0
lee,donghun <ldh0774@naver.com>
2022-03-29 23:19:27 +0900

    Implement timer

--- E3D_2_1_AutoSave/MainForm.Designer.cs
+++ E3D_2_1_AutoSave/MainForm.Designer.cs
@@ -29,33 +29,57 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Btn_Start = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Btn_Stop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 63);
+            this.label1.Location = new System.Drawing.Point(22, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
-            // button2
+            // Btn_Start
             // 
-            this.button2.Location = new System.Drawing.Point(97, 58);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.Btn_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Start.Location = new System.Drawing.Point(24, 71);
+            this.Btn_Start.Name = "Btn_Start";
+            this.Btn_Start.Size = new System.Drawing.Size(75, 34);
+            this.Btn_Start.TabIndex = 2;
+            this.Btn_Start.Text = "Start";
+            this.Btn_Start.UseVisualStyleBackColor = true;
+            this.Btn_Start.Click += new System.EventHandler(this.Start_Button_Event);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(3, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 1);
+            this.panel1.TabIndex = 3;
+            // 
+            // Btn_Stop
+            // 
+            this.Btn_Stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Stop.Location = new System.Drawing.Point(114, 71);
+            this.Btn_Stop.Name = "Btn_Stop";
+            this.Btn_Stop.Size = new System.Drawing.Size(75, 34);
+            this.Btn_Stop.TabIndex = 4;
+            this.Btn_Stop.Text = "Stop";
+            this.Btn_Stop.UseVisualStyleBackColor = true;
+            this.Btn_Stop.Click += new System.EventHandler(this.Btn_Stop_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Btn_Stop);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Btn_Start);
             this.Controls.Add(this.label1);
             this.Name = "MainForm";
             this.Size = new System.Drawing.Size(427, 580);
@@ -67,6 +91,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Btn_Start;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Btn_Stop;
     }
 }
--- E3D_2_1_AutoSave/MainForm.cs
+++ E3D_2_1_AutoSave/MainForm.cs
@@ -7,26 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Aveva.Core.Utilities.CommandLine;
+using System.Diagnostics;
 
 namespace AutoSave.ehdgns
 {
     public partial class MainForm : UserControl
     {
+        Timer timer = new Timer();
+        Stopwatch stopwatch = new Stopwatch();
         public MainForm()
         {
             InitializeComponent();
-
         }
-        void Save()
+        private void Start_Button_Event(object sender, EventArgs e)
+        {
+            stopwatch.Restart();
+            timer.Enabled = true;
+            timer.Interval = 10000;
+            timer.Tick += Timer_Tick;
+
+            
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
         {
             Command command = Command.CreateCommand("!!alert.message('aasfdsfd')");
             command.Run();
-
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Btn_Stop_Click(object sender, EventArgs e)
         {
-            Save();
+            timer.Enabled=false;
+            stopwatch.Stop();
         }
     }
 }
